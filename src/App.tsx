ff5a86417cc93e9ac669fdadb05dc13d39fae916
[hyunba] <dls272@naver.com>
2021-12-27 23:38:34 +0900

    problem

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./theme";
-import { useState } from "react";
+// import { ThemeProvider } from "styled-components";
+// import { darkTheme, lightTheme } from "./theme";
+// import { useState } from "react";
+import Circle from "./Circle";
 
 function App() {
-  const [isDark, setIsDark] = useState(false);
-  const toggleDark = () => setIsDark(current => !current);
+  // const [isDark, setIsDark] = useState(false);
+  // const toggleDark = () => setIsDark(current => !current);
   return (
-    <>
-      <ThemeProvider theme = {isDark ? darkTheme : lightTheme}>
-        <button onClick={toggleDark}>Toggle Mode</button>
-
-      </ThemeProvider>
-    </>
+    // <>
+    //   <ThemeProvider theme = {isDark ? darkTheme : lightTheme}>
+    //     <button onClick={toggleDark}>Toggle Mode</button>
+      <div>
+        <Circle/>
+      </div>
+    //   </ThemeProvider>
+    // </>
   );
 }
 
---
+++ src/Circle.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+// interface ContainerProps {
+//     // bgColor: string;
+// }
+
+const Container = styled.div`
+    width: 200px;
+    height: 200px;
+    
+`;
+
+// interface CircleProps {
+//     bgColor: string;
+// }
+
+function Circle() {
+    return (
+        // <Container/>
+        <div>hi</div>
+    );
+}
+
+export default Circle;
