42756586976a32948556c443615a6f05ebdfbc90
[hyunba] <dls272@naver.com>
2021-12-30 23:28:45 +0900

    styled-components error solve!

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -7,7 +7,7 @@ import styled from "styled-components";
 const Container = styled.div`
     width: 200px;
     height: 200px;
-    
+    background-color: teal;
 `;
 
 // interface CircleProps {
@@ -16,8 +16,7 @@ const Container = styled.div`
 
 function Circle() {
     return (
-        // <Container/>
-        <div>hi</div>
+        <div><Container/></div>
     );
 }
 
